373012f5ecc62867808297675fb339b6030dfd05
maneldev13 <manelgithub13@gmail.com>
2026-03-11 00:51:24 +0100

    t9addi tbddi frontend

--- DjangoProjects/guide_touristique/guide_touristique/urls.py
+++ DjangoProjects/guide_touristique/guide_touristique/urls.py
@@ -1,10 +1,18 @@
-from django.contrib import admin # pyright: ignore[reportMissingModuleSource]
-from django.urls import path, include # pyright: ignore[reportMissingModuleSource]
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'home.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('', home),
+
     path('hotels/', include('hotels.urls')),
+
     path('places/', include('places.urls')),
+
     path('restaurants/', include('restaurants.urls')),
 ]
